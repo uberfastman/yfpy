ebe500f08177cb801463f9297398717036fdc8cf
uberfastman <4575707+uberfastman@users.noreply.github.com>
2019-09-09 11:24:24 -0400

    update pypi version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as reqs:
 
 setuptools.setup(
     name="yffpy",
-    version="1.1.0",
+    version="1.1.1",
     author="Wren J. R.",
     author_email="wrenjr@yahoo.com",
     description="Python API wrapper for the Yahoo Fantasy Football public API.",
