80692c714a0e6bb04fb673b4d44008f020cc287e
uberfastman <4575707+uberfastman@users.noreply.github.com>
2022-05-12 15:35:36 -0400

    update Makefile and sphinx documentation

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v8.0.0',
+    VERSION: 'v8.0.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
