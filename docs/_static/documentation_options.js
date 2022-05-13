95e85959dc7affb818e6a3cc589f8b13eb7913ed
uberfastman <4575707+uberfastman@users.noreply.github.com>
2022-05-13 18:49:06 -0400

    update sphinx documentation

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v8.0.5',
+    VERSION: 'v8.0.6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
