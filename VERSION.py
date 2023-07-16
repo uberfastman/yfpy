df1c3ba6b876e5718c5e8d49f706084d5a2c0c20
uberfastman <4575707+uberfastman@users.noreply.github.com>
2023-07-16 19:17:18 -0400

    update version number and docs

--- VERSION.py
+++ VERSION.py
@@ -1,2 +1,2 @@
 # DO NOT EDIT - VERSIONING CONTROLLED BY GIT TAGS
-__version__ = "v10.0.2"
+__version__ = "v10.0.3"
--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v10.0.2',
+    VERSION: 'v10.0.3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
