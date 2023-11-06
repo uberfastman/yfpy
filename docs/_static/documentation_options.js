ae0ba09bfd430bb60f8510d2448bd634598e90eb
uberfastman <4575707+uberfastman@users.noreply.github.com>
2023-11-06 17:54:06 -0500

    update version number and docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v15.0.0',
+    VERSION: 'v15.0.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
